85f53caa128908beb4b9b20579bf5800fbdd8b40
Gevaset <newgevaset@gmail.com>
2020-10-13 22:13:48 +0530

    Updates

---
+++ precache-manifest.54f7c4210ed8653044cc3b84efe00581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8d9c470125775802d10fe4f3e73f84",
+    "url": "/Chatty/index.html"
+  },
+  {
+    "revision": "53fc669825ac66425206",
+    "url": "/Chatty/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "929d88b6fc7a1c0c74a4",
+    "url": "/Chatty/static/css/main.f00fff3a.chunk.css"
+  },
+  {
+    "revision": "53fc669825ac66425206",
+    "url": "/Chatty/static/js/2.abd4812c.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/Chatty/static/js/2.abd4812c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929d88b6fc7a1c0c74a4",
+    "url": "/Chatty/static/js/main.feb4e550.chunk.js"
+  },
+  {
+    "revision": "aa0560b3f6ac4465f113",
+    "url": "/Chatty/static/js/runtime-main.9b0f9503.js"
+  }
+]);
